ae19432f56e7ab2952e02d0a5d21eda945c6ca60
Q <q@misell.cymru>
2024-01-26 10:27:59 +0000

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python3 manage.py collectstatic --noinput || exit 1
 python3 manage.py migrate || exit 1
 
-exec gunicorn -w 4 -b [::]:8000 --forwarded-allow-ips * --access-logfile - --log-level=info --timeout=90 emf_bus_tracking.wsgi:application
+exec gunicorn -w 4 -b [::]:8000 --forwarded-allow-ips \* --access-logfile - --log-level=info --timeout=90 emf_bus_tracking.wsgi:application
